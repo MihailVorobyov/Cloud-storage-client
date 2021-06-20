f605baa0f9d67732bc8f4b78d6f55e5d1ad4326f
Mihail <ptichkinm@mail.ru>
2021-06-20 21:12:50 +0300

    GUI released with javax.swing

--- src/main/java/com/polozov/cloudstorage/lesson01/Client.java
+++ src/main/java/com/polozov/cloudstorage/lesson01/Client.java
@@ -1,55 +1,177 @@
 package com.polozov.cloudstorage.lesson01;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Client extends JFrame {
+	private final String ICON_PATH = "src" + File.separator + "images" + File.separator + "32x32" + File.separator;
+	
 	private final Socket socket;
 	private final DataOutputStream out;
 	private final DataInputStream in;
-
+	
+	private String[] buttonsNames = {
+		"Download", "Upload", "Copy", "Paste", "Cut", "Delete", "Rename", "Search", "Make_dir"
+	};
+	private JButton[] buttons = new JButton[buttonsNames.length];
+	
+	private final int WINDOW_BORDER_SIZE = this.getInsets().left;
+	private final int WINDOW_HEAD_HEIGHT = this.getInsets().bottom;
+	
+	private Dimension listPreferredSize = new Dimension(350, 450);
+	private Dimension windowSize = new Dimension(2 * WINDOW_BORDER_SIZE + 2 * listPreferredSize.width + 6 * 8 + 32,
+		listPreferredSize.height + WINDOW_BORDER_SIZE * 8 + WINDOW_HEAD_HEIGHT + 120);
+	
 	public Client() throws IOException {
 		socket = new Socket("localhost", 5678);
 		out = new DataOutputStream(socket.getOutputStream());
 		in = new DataInputStream(socket.getInputStream());
 
-		setSize(300, 300);
-		JPanel panel = new JPanel(new GridLayout(2, 1));
+		// ---------- Frame ----------
 
+		this.setSize(windowSize);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// ---------- Меню ----------
+		
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu[] menus = {
+				new JMenu("File"),
+				new JMenu("Edit"),
+				new JMenu("About")
+		};
+		
+		JMenuItem[][] menuItems = {
+				{   // File
+						new JMenuItem("Exit"),
+				},
+				{   // Edit
+						new JMenuItem("Copy"),
+						new JMenuItem("Paste"),
+						new JMenuItem("Paste"),
+						new JMenuItem("Delete")
+				},
+				{   // Help
+						new JMenuItem("About application")
+				}
+		};
+		
+		for (int i = 0; i < menus.length; i++) {
+			menuBar.add(menus[i]);
+			for (int j = 0; j < menuItems[i].length; j++) {
+				menus[i].add(menuItems[i][j]);
+			}
+		}
+		
+		// ---------- Панели ----------
+		
+		JPanel serverPanel = new JPanel();
+		serverPanel.setLayout(new BoxLayout(serverPanel, BoxLayout.Y_AXIS));
+//		JList serverList = new JList();
+//		serverList.setPreferredSize(listPreferredSize);
+//		serverList.setListData(new File("server").list());
+//		serverPanel.add(serverList);
+		
+		// Таблица с заголовком
+		String[] columns = {"Name", "Type", "Size", "Change date"};
+		File dirContent = new File("server");
+		File[] files = dirContent.listFiles();
+		String[][] serverTableData = new String[dirContent.listFiles().length][columns.length];
+		
+		for (int i = 0; i < dirContent.list().length; i ++) {
+			String[] fileName = files[i].getName().split("\\.", 2);
+			serverTableData[i][0] = fileName[0];
+			serverTableData[i][1] = fileName[1];
+			serverTableData[i][2] = files[i].length() + " bytes";
+			
+			// TODO: дату создания/изменения хранить в БД
+//			serverTableData[i][3] =
+		}
+		JTable serverFilesTable = new JTable(serverTableData, columns);
+		serverFilesTable.setRowSelectionAllowed(true);
+		serverFilesTable.setShowGrid(false);
+		serverFilesTable.getTableHeader().setReorderingAllowed(false);
+		serverPanel.add(serverFilesTable.getTableHeader());
+		serverPanel.add(serverFilesTable);
+
+		
+ 		JPanel centralPanel = new JPanel();
+ 		centralPanel.setLayout(new BoxLayout(centralPanel, BoxLayout.Y_AXIS));
+//		centralPanel.setBackground(Color.yellow);
+		
+		JPanel userPanel = new JPanel();
+//		userPanel.setBackground(Color.green);
+		JList userList = new JList();
+		userList.setPreferredSize(listPreferredSize);
+		userList.setListData(new File("client").list());
+		userPanel.add(userList);
+		
+		JPanel southPanel = new JPanel();
+		
+		// ---------- Кнопки ----------
+		
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i] = new JButton();
+			buttons[i].setMargin(new Insets(0, 0, 0, 0));
+			buttons[i].setIcon(new ImageIcon(ICON_PATH + buttonsNames[i] + ".png"));
+			centralPanel.add(buttons[i]);
+			buttons[i].setAlignmentX(Component.CENTER_ALIGNMENT);
+			buttons[i].setAlignmentY(Component.CENTER_ALIGNMENT);
+			if (i < buttons.length - 1) {
+				centralPanel.add(new JSeparator());
+			}
+		}
+		
+		// ---------- Разное ----------
+		
 		JButton btnSend = new JButton("SEND");
-		JTextField textField = new JTextField();
+		JTextField textField = new JTextField(20);
 
 		btnSend.addActionListener(a -> {
-			// upload 1.txt
-			// download img.png
-			String[] cmd = textField.getText().split(" ", 2); // limit: 2 добавлено для корректной работы с файлами,
-																		// содержащими пробелы в названии
+			// limit: 2 позволяет корректно работать с файлами, содержащими пробелы в названии
+			String[] cmd = textField.getText().split(" ", 2);
+			
 			if ("upload".equals(cmd[0])) {
 				sendFile(cmd[1]);
 			} else if ("download".equals(cmd[0])) {
 				getFile(cmd[1]);
 			}
 		});
-
-		panel.add(textField);
-		panel.add(btnSend);
-
-		add(panel);
-
-		addWindowListener(new WindowAdapter() {
+		
+		this.getContentPane().add(BorderLayout.NORTH, menuBar);
+		this.getContentPane().add(BorderLayout.WEST, serverPanel);
+		this.getContentPane().add(BorderLayout.EAST, userPanel);
+		this.getContentPane().add(BorderLayout.CENTER, centralPanel);
+		this.getContentPane().add(BorderLayout.SOUTH, southPanel);
+		
+		southPanel.add(textField);
+		southPanel.add(btnSend);
+		
+		
+		this.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				super.windowClosing(e);
 				sendMessage("exit");
 			}
 		});
-
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
+		
+		this.setVisible(true);
 	}
 
 	private void getFile(String filename) {
